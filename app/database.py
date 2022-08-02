a68e7bef0b38613c2ddac1eec90f5ca5ad3a8695
ts15 <ts15@illinois.edu>
2022-08-02 10:26:09 -0500

    first commit

---
+++ app/database.py
@@ -0,0 +1,93 @@
+"""Defines all the functions related to the database"""
+from app import db
+
+def fetch_todo() -> dict:
+    """Reads all tasks listed in the todo table
+
+    Returns:
+        A list of dictionaries
+    """
+
+    conn = db.connect()
+    query_results = conn.execute("Select * from Job_Description limit 50;").fetchall()
+    conn.close()
+    todo_list = []
+    for result in query_results:
+        item = {
+            "id": result[0],
+            "task": result[1],
+            "status": result[2],
+            "rating": result[3]
+        }
+        todo_list.append(item)
+
+    return todo_list
+
+def update_job_description(job_id: int, data: dict) -> None:
+    conn = db.connect()
+    query = 'Update Job_Description set Job_Title = "{}" where id = {};'.format(data['description'],job_id)
+    conn.execute(query)
+    conn.close()
+
+def update_task_entry(task_id: int, text: str) -> None:
+    """Updates task description based on given `task_id`
+
+    Args:
+        task_id (int): Targeted task_id
+        text (str): Updated description
+
+    Returns:
+        None
+    """
+
+    conn = db.connect()
+    query = 'Update Job_Description set task = "{}" where id = {};'.format(text, task_id)
+    conn.execute(query)
+    conn.close()
+
+
+def update_status_entry(task_id: int, text: str) -> None:
+    """Updates task status based on given `task_id`
+
+    Args:
+        task_id (int): Targeted task_id
+        text (str): Updated status
+
+    Returns:
+        None
+    """
+
+    conn = db.connect()
+    query = 'Update tasks set status = "{}" where id = {};'.format(text, task_id)
+    conn.execute(query)
+    conn.close()
+
+
+def insert_new_posting(data: dict) ->  int:
+    """Insert new task to todo table.
+
+    Args:
+        text (str): Task description
+
+    Returns: The task ID for the inserted entry
+    """
+
+    conn = db.connect()
+    jobid = 0
+    rate = 5
+    query = 'Insert Into Job_Description (Job_ID, Job_Title, Company_Name, Rating) VALUES ("{}", "{}", "{}", "{}");'.format(int(data['id']), data['description'], data['companyname'], int(data['rating']))
+    conn.execute(query)
+    query_results = conn.execute("Select LAST_INSERT_ID();")
+    query_results = [x for x in query_results]
+    task_id = query_results[0][0]
+    conn.close()
+
+    return task_id
+
+
+def remove_task_by_id(task_id: int) -> None:
+    """ remove entries based on task ID """
+    conn = db.connect()
+    query = 'Delete From Job_Description where Job_id={};'.format(task_id)
+    conn.execute(query)
+    conn.close()
